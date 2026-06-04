765ca7a283d745701557b128e6fd1fcd76407d43
Arvind <palshahab1290@gmail.com>
2026-06-04 19:02:51 +0530

    hee new commit

---
+++ Hello.js
@@ -0,0 +1,3 @@
+console.log("hello node js im  arvind");
+// inside of nodejs not excuted a window relted function 
+// always start a nodejs project fisrt is initilize in your project type in terminal npm init
